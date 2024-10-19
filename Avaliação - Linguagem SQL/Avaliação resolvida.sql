da755e52aff5110ae87edb6345705cfd2f121f29
Mauros Milach Junior <mauros99milach@gmail.com>
2024-10-19 18:46:02 -0300

    feat: Avaliação resolvida.sql

--- Avaliação - Linguagem SQL/Avaliação resolvida.sql
+++ Avaliação - Linguagem SQL/Avaliação resolvida.sql
@@ -70,59 +70,308 @@ ALTER TABLE filme_ator
         
 -- Criando Sequences
 CREATE SEQUENCE genero_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCYCLE CACHE 20;
+
 CREATE SEQUENCE ator_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCYCLE CACHE 20;
+
 CREATE SEQUENCE censure_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCYCLE CACHE 20;
+
 CREATE SEQUENCE filme_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCYCLE CACHE 20;
 
 --(1,0 Ponto) Inserção de, no mínimo: 05 filmes, 05 atores, 03 gêneros e 03 censuras - todos (atores, gêneros e censuras associados a filmes);
 
 -- Inserindo Gêneros
-INSERT INTO genero (cd_genero, ds_genero) VALUES (genero_seq.NEXTVAL, 'Ação');
-INSERT INTO genero (cd_genero, ds_genero) VALUES (genero_seq.NEXTVAL, 'Comédia');
-INSERT INTO genero (cd_genero, ds_genero) VALUES (genero_seq.NEXTVAL, 'Drama');
+INSERT INTO genero (
+    cd_genero,
+    ds_genero
+) VALUES (
+    genero_seq.NEXTVAL,
+    'Ação'
+);
+
+INSERT INTO genero (
+    cd_genero,
+    ds_genero
+) VALUES (
+    genero_seq.NEXTVAL,
+    'Comédia'
+);
+
+INSERT INTO genero (
+    cd_genero,
+    ds_genero
+) VALUES (
+    genero_seq.NEXTVAL,
+    'Drama'
+);
 
 -- Inserindo Censuras
-INSERT INTO censura (cd_censura, ds_censura) VALUES (censure_seq.NEXTVAL, 'Livre');
-INSERT INTO censura (cd_censura, ds_censura) VALUES (censure_seq.NEXTVAL, '12 anos');
-INSERT INTO censura (cd_censura, ds_censura) VALUES (censure_seq.NEXTVAL, '16 anos');
+INSERT INTO censura (
+    cd_censura,
+    ds_censura
+) VALUES (
+    censure_seq.NEXTVAL,
+    'Livre'
+);
+
+INSERT INTO censura (
+    cd_censura,
+    ds_censura
+) VALUES (
+    censure_seq.NEXTVAL,
+    '12 anos'
+);
+
+INSERT INTO censura (
+    cd_censura,
+    ds_censura
+) VALUES (
+    censure_seq.NEXTVAL,
+    '16 anos'
+);
 
 -- Inserindo Atores
-INSERT INTO ator (cd_ator, nm_ator) VALUES (ator_seq.NEXTVAL, 'Ator 1');
-INSERT INTO ator (cd_ator, nm_ator) VALUES (ator_seq.NEXTVAL, 'Ator 2');
-INSERT INTO ator (cd_ator, nm_ator) VALUES (ator_seq.NEXTVAL, 'Ator 3');
-INSERT INTO ator (cd_ator, nm_ator) VALUES (ator_seq.NEXTVAL, 'Ator 4');
-INSERT INTO ator (cd_ator, nm_ator) VALUES (ator_seq.NEXTVAL, 'Ator 5');
+INSERT INTO ator (
+    cd_ator,
+    nm_ator
+) VALUES (
+    ator_seq.NEXTVAL,
+    'Ator 1'
+);
+
+INSERT INTO ator (
+    cd_ator,
+    nm_ator
+) VALUES (
+    ator_seq.NEXTVAL,
+    'Ator 2'
+);
+
+INSERT INTO ator (
+    cd_ator,
+    nm_ator
+) VALUES (
+    ator_seq.NEXTVAL,
+    'Ator 3'
+);
+
+INSERT INTO ator (
+    cd_ator,
+    nm_ator
+) VALUES (
+    ator_seq.NEXTVAL,
+    'Ator 4'
+);
+
+INSERT INTO ator (
+    cd_ator,
+    nm_ator
+) VALUES (
+    ator_seq.NEXTVAL,
+    'Ator 5'
+);
 
 -- Inserindo Filmes
-INSERT INTO filme (cd_filme, cd_censura, cd_genero, nm_filme, ds_sinopse, nr_duracao, dt_lancamento, fl_dublado) 
-VALUES (filme_seq.NEXTVAL, 1, 1, 'Filme 1', 'Sinopse do Filme 1', '120 min', TO_DATE('2024-01-01', 'YYYY-MM-DD'), 'S');
+INSERT INTO filme (
+    cd_filme,
+    cd_censura,
+    cd_genero,
+    nm_filme,
+    ds_sinopse,
+    nr_duracao,
+    dt_lancamento,
+    fl_dublado
+) VALUES (
+    filme_seq.NEXTVAL,
+    1,
+    1,
+    'Filme 1',
+    'Sinopse do Filme 1',
+    '120 min',
+    TO_DATE('2024-01-01', 'YYYY-MM-DD'),
+    'S'
+);
 
-INSERT INTO filme (cd_filme, cd_censura, cd_genero, nm_filme, ds_sinopse, nr_duracao, dt_lancamento, fl_dublado) 
-VALUES (filme_seq.NEXTVAL, 2, 2, 'Filme 2', 'Sinopse do Filme 2', '90 min', TO_DATE('2024-02-01', 'YYYY-MM-DD'), 'N');
+INSERT INTO filme (
+    cd_filme,
+    cd_censura,
+    cd_genero,
+    nm_filme,
+    ds_sinopse,
+    nr_duracao,
+    dt_lancamento,
+    fl_dublado
+) VALUES (
+    filme_seq.NEXTVAL,
+    2,
+    2,
+    'Filme 2',
+    'Sinopse do Filme 2',
+    '90 min',
+    TO_DATE('2024-02-01', 'YYYY-MM-DD'),
+    'N'
+);
 
-INSERT INTO filme (cd_filme, cd_censura, cd_genero, nm_filme, ds_sinopse, nr_duracao, dt_lancamento, fl_dublado) 
-VALUES (filme_seq.NEXTVAL, 3, 3, 'Filme 3', 'Sinopse do Filme 3', '150 min', TO_DATE('2024-03-01', 'YYYY-MM-DD'), 'S');
+INSERT INTO filme (
+    cd_filme,
+    cd_censura,
+    cd_genero,
+    nm_filme,
+    ds_sinopse,
+    nr_duracao,
+    dt_lancamento,
+    fl_dublado
+) VALUES (
+    filme_seq.NEXTVAL,
+    3,
+    3,
+    'Filme 3',
+    'Sinopse do Filme 3',
+    '150 min',
+    TO_DATE('2024-03-01', 'YYYY-MM-DD'),
+    'S'
+);
 
-INSERT INTO filme (cd_filme, cd_censura, cd_genero, nm_filme, ds_sinopse, nr_duracao, dt_lancamento, fl_dublado) 
-VALUES (filme_seq.NEXTVAL, 1, 1, 'Filme 4', 'Sinopse do Filme 4', '110 min', TO_DATE('2024-04-01', 'YYYY-MM-DD'), 'N');
+INSERT INTO filme (
+    cd_filme,
+    cd_censura,
+    cd_genero,
+    nm_filme,
+    ds_sinopse,
+    nr_duracao,
+    dt_lancamento,
+    fl_dublado
+) VALUES (
+    filme_seq.NEXTVAL,
+    1,
+    1,
+    'Filme 4',
+    'Sinopse do Filme 4',
+    '110 min',
+    TO_DATE('2024-04-01', 'YYYY-MM-DD'),
+    'N'
+);
 
-INSERT INTO filme (cd_filme, cd_censura, cd_genero, nm_filme, ds_sinopse, nr_duracao, dt_lancamento, fl_dublado) 
-VALUES (filme_seq.NEXTVAL, 2, 2, 'Filme 5', 'Sinopse do Filme 5', '130 min', TO_DATE('2024-05-01', 'YYYY-MM-DD'), 'S');
+INSERT INTO filme (
+    cd_filme,
+    cd_censura,
+    cd_genero,
+    nm_filme,
+    ds_sinopse,
+    nr_duracao,
+    dt_lancamento,
+    fl_dublado
+) VALUES (
+    filme_seq.NEXTVAL,
+    2,
+    2,
+    'Filme 5',
+    'Sinopse do Filme 5',
+    '130 min',
+    TO_DATE('2024-05-01', 'YYYY-MM-DD'),
+    'S'
+);
 
 -- Associando Atores aos Filmes
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (1, 1);
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (1, 2);
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (2, 3);
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (3, 4);
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (4, 5);
-INSERT INTO filme_ator (cd_filme, cd_ator) VALUES (5, 1);
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    1,
+    1
+);
+
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    1,
+    2
+);
+
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    2,
+    3
+);
+
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    3,
+    4
+);
+
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    4,
+    5
+);
+
+INSERT INTO filme_ator (
+    cd_filme,
+    cd_ator
+) VALUES (
+    5,
+    1
+);
 
 COMMIT;
 
 --(1.0 Ponto) Exibir as informações sobre os filmes (nome, descrição do gênero e cencura, duração e indicação de dublagem ou não), assim como o(s) ator(es) envolvido(s) no respectivo filme;
+SELECT
+    f.nm_filme   AS "Nome do Filme",
+    g.ds_genero  AS "Genero",
+    c.ds_censura AS "Censura",
+    f.nr_duracao AS "Duração",
+    f.fl_dublado AS "Dublado",
+    a.nm_ator    AS "Nome do(s) ator(es)"
+FROM
+    filme      f,
+    genero     g,
+    censura    c,
+    filme_ator fa,
+    ator       a
+WHERE
+    ( g.cd_genero = f.cd_genero )
+    AND ( f.cd_censura = c.cd_censura )
+    AND ( f.cd_filme = fa.cd_filme )
+    AND ( fa.cd_ator = a.cd_ator );
 
 --(1,0 Ponto) Listagem dos nomes dos filmes, com a respectiva descrição da censura e a quantidade de atores cadastrados;
+SELECT
+    f.nm_filme                 "Nome do filme",
+    c.ds_censura               AS "Censura",
+    COUNT(DISTINCT fa.cd_ator) AS "Quantidade Ator(es)"
+FROM
+    filme      f,
+    censura    c,
+    filme_ator fa
+WHERE
+    ( f.cd_censura = c.cd_censura )
+    AND ( f.cd_filme = fa.cd_filme )
+GROUP BY
+    f.nm_filme,
+    c.ds_censura
+ORDER BY
+    3 DESC;
 
 --(1,0 Ponto) Recuperação das descrições dos gêneros com a respectiva quantidade de filmes agrupada a cada gênero;
+SELECT
+    g.ds_genero       AS "Genero",
+    COUNT(f.cd_filme) AS "Quantidade por Genero"
+FROM
+    censura c,
+    filme   f,
+    genero  g
+WHERE
+    ( f.cd_censura = c.cd_censura )
+    AND ( f.cd_genero = g.cd_genero )
+GROUP BY
+    g.ds_genero;
 
 --(1,5 Pontos) Exibir o(s) nome(s) do(s) ator(es) que não está(ão) associado(s) a nenhum filme em que a data de lançamento do filme tenha sido no ano de 2019.
